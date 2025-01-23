b0966e04f0a3edc248ed02b81d3a3d0b7ce688ed
KnoX <102421933+7urn@users.noreply.github.com>
2025-01-24 00:34:04 +0100

    Add files via upload

---
+++ config.js
@@ -0,0 +1,46 @@
+
+
+module.exports = {
+  TOKEN: "",
+  language: "it",
+  ownerID: ["585500027066253313", "638261385750642700", "635062507291148288"], 
+  mongodbUri : "mongodb+srv://EvanZ:EvanZccc@cluster0.kx6iu.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",
+  setupFilePath: './commands/setup.json',
+  commandsDir: './commands',  
+  embedColor: "#1db954",
+  activityName: "Hino", 
+  activityType: "LISTENING",  // Available activity types : LISTENING , PLAYING
+  SupportServer: "https://discord.gg/43VDd3UTrM",
+  embedTimeout: 5, 
+  errorLog: "", 
+  nodes: [
+    {
+      name: "Koi Node V4",
+      password: "prplmoe.me",
+      host: "lavav4.prplmoe.me",
+      port: 1118,
+      secure: false
+    },
+    {
+      name: "INZEWORLD.COM (DE)",
+      password: "saher.inzeworld.com",
+      host: "lava.inzeworld.com",
+      port: 3128,
+      secure: false
+    },
+    {
+      name: "Catfein ID",
+      password: "catfein",
+      host: "lava.catfein.com",
+      port: 4000,
+      secure: false
+    },
+    { 
+      name: "saher",
+      password: "saher.inzeworld.com",
+      host: "lava.inzeworld.com",
+      port: 3128,
+      secure: false
+    }
+  ]
+}
