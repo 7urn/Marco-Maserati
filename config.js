e37377091a793d8196af4055a47ce657d569ff5f
KnoX <102421933+7urn@users.noreply.github.com>
2025-03-18 03:33:27 +0100

    config.js

--- config.js
+++ config.js
@@ -16,10 +16,10 @@ module.exports = {
    nodes: [
      
 {
-  identifier: "ChalresNaig Node",
-  password: "NAIGLAVA-dash.techbyte.host",
-  host: "lavahatry4.techbyte.host",
-  port: 3000,
+  identifier: "Public Lavalink v4 NonSSL",
+  password: "https://dsc.gg/ajidevserver",
+  host: "lava-v4.ajieblogs.eu.org",
+  port: 80,
   secure: false
 }
   ]
